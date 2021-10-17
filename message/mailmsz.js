82650ed25c384e9147d51be526c59801b137e223
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2021-10-17 22:43:06 +0530

    Add files via upload

---
+++ message/mailmsz.js
@@ -0,0 +1,9 @@
+const message = {
+    from: "saurabh@gmail.com",
+    to: "djawaha@gmail.com",
+    subject: " we are working on nodemailer tis is new way to send msg ",
+    text: " its working is very nice way ",
+    html: "<p>dj is sap sd developer</p>"
+  };
+
+  module.exports =message;
